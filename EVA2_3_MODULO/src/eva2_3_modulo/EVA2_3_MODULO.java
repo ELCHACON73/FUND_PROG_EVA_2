e6420e50fe6bcc2b8bf5d5333b19b72c220af2c1
ELCHACON73 <143569013+ELCHACON73@users.noreply.github.com>
2023-10-16 18:16:22 -0700

    Add files via upload

---
+++ EVA2_3_MODULO/src/eva2_3_modulo/EVA2_3_MODULO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_3_modulo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_3_MODULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor, residuo;
+        Scanner input = new Scanner(System.in);
+        System.out.println("introduce el valor a evaluar:"); 
+        valor = input.nextInt();
+        
+        residuo = valor & 2; //& módulo --> residuo
+        if(residuo == 0) //VALOR ES PAR
+            System.out.println("EL VALOR ES PAR");
+        else
+            System.out.println("EL VALOR ES IMPAR");
+    }
+    
+}
